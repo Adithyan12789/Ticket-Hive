cd037537848a70d7f12f39a99edac7e263b0bb29
Adithyan <Adithiruthiparambil12@gmail.com>
2024-12-27 12:27:48 +0530

    init commit

--- Back-End/Utils/GenerateTheaterToken.ts
+++ Back-End/Utils/GenerateTheaterToken.ts
@@ -2,26 +2,45 @@ import { Response } from 'express';
 import jwt from 'jsonwebtoken';
 
 class TheaterTokenService {
-    private jwtSecret: string;
-    private jwts = process.env.JWT_SECRET_THEATER || 'metasploit192167';
+    private readonly jwtSecret: string;
 
     constructor() {
-        if (!this.jwts) {
-            throw new Error('JWT_SECRET_THEATER is not defined');
+        const jwtSecret = process.env.JWT_SECRET_THEATER;
+        if (!jwtSecret) {
+            throw new Error('Environment variable JWT_SECRET_THEATER is not defined');
         }
-        this.jwtSecret = this.jwts;
+        this.jwtSecret = jwtSecret;
     }
 
-    public generateTheaterToken(res: Response, theaterOwnerId: string): void {
-        const token = jwt.sign({ id: theaterOwnerId }, this.jwtSecret, { expiresIn: '30d' });
+    public generateTheaterToken(res: Response, theaterOwnerId: string, expiresIn: string = '30d'): void {
+        const token = jwt.sign({ id: theaterOwnerId }, this.jwtSecret, { expiresIn });
 
         res.cookie('theaterOwnerJwt', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
+            secure: process.env.NODE_ENV === 'production',
             sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000,
+            maxAge: this.getMaxAge(expiresIn),
         });
+    }
+
+    private getMaxAge(expiresIn: string): number {
+        const match = expiresIn.match(/^(\d+)([dhms])$/);
+        if (!match) {
+            throw new Error('Invalid expiresIn format. Use a number followed by "d", "h", "m", or "s".');
+        }
+
+        const [, value, unit] = match;
+        const numValue = parseInt(value, 10);
+
+        switch (unit) {
+            case 'd': return numValue * 24 * 60 * 60 * 1000;
+            case 'h': return numValue * 60 * 60 * 1000;
+            case 'm': return numValue * 60 * 1000;
+            case 's': return numValue * 1000;
+            default: throw new Error('Invalid time unit. Use "d", "h", "m", or "s".');
+        }
     }
 }
 
 export default new TheaterTokenService();
+
--- dist/Utils/GenerateTheaterToken.js
+++ dist/Utils/GenerateTheaterToken.js
@@ -6,20 +6,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 class TheaterTokenService {
     constructor() {
-        this.jwts = process.env.JWT_SECRET_THEATER || 'metasploit192167';
-        if (!this.jwts) {
-            throw new Error('JWT_SECRET_THEATER is not defined');
+        const jwtSecret = process.env.JWT_SECRET_THEATER;
+        if (!jwtSecret) {
+            throw new Error('Environment variable JWT_SECRET_THEATER is not defined');
         }
-        this.jwtSecret = this.jwts;
+        this.jwtSecret = jwtSecret;
     }
-    generateTheaterToken(res, theaterOwnerId) {
-        const token = jsonwebtoken_1.default.sign({ id: theaterOwnerId }, this.jwtSecret, { expiresIn: '30d' });
+    generateTheaterToken(res, theaterOwnerId, expiresIn = '30d') {
+        const token = jsonwebtoken_1.default.sign({ id: theaterOwnerId }, this.jwtSecret, { expiresIn });
         res.cookie('theaterOwnerJwt', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
+            secure: process.env.NODE_ENV === 'production',
             sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000,
+            maxAge: this.getMaxAge(expiresIn),
         });
+    }
+    getMaxAge(expiresIn) {
+        const match = expiresIn.match(/^(\d+)([dhms])$/);
+        if (!match) {
+            throw new Error('Invalid expiresIn format. Use a number followed by "d", "h", "m", or "s".');
+        }
+        const [, value, unit] = match;
+        const numValue = parseInt(value, 10);
+        switch (unit) {
+            case 'd': return numValue * 24 * 60 * 60 * 1000;
+            case 'h': return numValue * 60 * 60 * 1000;
+            case 'm': return numValue * 60 * 1000;
+            case 's': return numValue * 1000;
+            default: throw new Error('Invalid time unit. Use "d", "h", "m", or "s".');
+        }
     }
 }
 exports.default = new TheaterTokenService();
