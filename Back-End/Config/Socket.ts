79a9bb7dde7cc561742110497924c65b6a6c9bdb
Adithyan <Adithiruthiparambil12@gmail.com>
2025-01-01 13:33:04 +0530

    init comm

--- Back-End/Config/Socket.ts
+++ Back-End/Config/Socket.ts
@@ -5,7 +5,7 @@ import http from "http";
 
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
+const io = new Server(server, {
   cors: {
     origin: 'https://ticket-hive-zeta.vercel.app',
     methods: ['GET', 'POST'],
