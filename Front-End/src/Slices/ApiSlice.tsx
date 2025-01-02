b97dc7a22fdaceef8bbb082f545b14efa54308d2
Adithyan <Adithiruthiparambil12@gmail.com>
2025-01-02 10:54:00 +0530

    init commit

--- Front-End/src/Slices/ApiSlice.tsx
+++ Front-End/src/Slices/ApiSlice.tsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery, BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query/react';
 
 const baseQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = fetchBaseQuery({ 
-  baseUrl: 'https://tickethive.fun/',
+  baseUrl: 'https://tickethive.fun/api/users/get-movies',
   credentials: 'include',
 });
 
