60f641d056b6ce4024612d7e976b5eb2ac890371
Adithyan <Adithiruthiparambil12@gmail.com>
2025-01-03 21:26:32 +0530

    init cmt

--- Front-End/src/Components/TheaterComponents/TheaterHeader.tsx
+++ Front-End/src/Components/TheaterComponents/TheaterHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
@@ -6,92 +6,19 @@ import { LinkContainer } from "react-router-bootstrap";
 import {
   useGetTheaterOwnerProfileQuery,
   useLogoutTheaterMutation,
-  useFetchUnreadNotificationsQuery,
-  useMarkNotificationAsReadMutation,
 } from "../../Slices/TheaterApiSlice";
 import { clearTheater } from "../../Slices/TheaterAuthSlice";
 import { useNavigate } from "react-router-dom";
 import { RootState, AppDispatch } from "../../Store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faSignOutAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import io from "socket.io-client";
-
-const socket = io("https://api.tickethive.fun");
-
-interface Notification {
-  _id: string;
-  message: string;
-  createdAt: string;
-}
+import { faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Header: React.FC = () => {
   const { theaterInfo } = useSelector((state: RootState) => state.theaterAuth);
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
-  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const { data: profileData } = useGetTheaterOwnerProfileQuery(theaterInfo?.id);
-  const { data: notifications = [], refetch } = useFetchUnreadNotificationsQuery({});
-  const [markNotificationAsRead] = useMarkNotificationAsReadMutation();
-  const [showNotifications, setShowNotifications] = useState(false);
-
-  console.log("notifications: ", notifications);
-  console.log("markNotificationAsRead: ", markNotificationAsRead);
-  
-
-  const handleNotificationClick = async (id: string) => {
-    try {
-      await markNotificationAsRead(id).unwrap();
-      refetch();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleIconClick = () => {
-    setShowNotifications(!showNotifications);
-  };
-
-  useEffect(() => {
-    const handleClickOutside = async (event: MouseEvent) => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setShowNotifications(false);
-        try {
-          await Promise.all(
-            notifications.map((notification: Notification) =>
-              markNotificationAsRead(notification._id).unwrap()
-            )
-          );
-          refetch();
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    };
-
-    if (showNotifications) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [showNotifications, notifications, markNotificationAsRead, refetch]);
-
-  useEffect(() => {
-    socket.on("newNotification", () => {
-      refetch();
-    });
-
-    return () => {
-      socket.off("newNotification");
-    };
-  }, [refetch]);
 
   const [logoutApiCall] = useLogoutTheaterMutation();
 
@@ -134,75 +61,6 @@ const Header: React.FC = () => {
             <Nav className="ms-auto">
               {theaterInfo ? (
                 <>
-                  <div className="notification-container" style={{ position: "relative", display: "inline-block" }}>
-                    <FontAwesomeIcon
-                      icon={faEnvelope}
-                      style={{
-                        color: showNotifications ? "#36a2eb" : "white",
-                        cursor: "pointer",
-                        marginTop: "10px",
-                        marginRight: "20px",
-                        fontSize: "23px",
-                      }}
-                      onClick={handleIconClick}
-                    />
-                    {notifications.length > 0 && (
-                      <span
-                        className="notification-count"
-                        style={{
-                          position: "absolute",
-                          top: "-5px",
-                          right: "15px",
-                          background: "red",
-                          color: "white",
-                          fontSize: "12px",
-                          padding: "2px 6px",
-                          borderRadius: "50%",
-                        }}
-                      >
-                        {notifications.length}
-                      </span>
-                    )}
-                  </div>
-
-                  {showNotifications && (
-                    <div
-                      className="notification-dropdown"
-                      ref={dropdownRef}
-                      style={{
-                        position: "absolute",
-                        top: "70px",
-                        right: "165px",
-                        backgroundColor: "#333",
-                        color: "white",
-                        padding: "10px",
-                        borderRadius: "5px",
-                        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-                        zIndex: 1000,
-                      }}
-                    >
-                      {notifications.map((notification: Notification) => (
-                        <div
-                          key={notification._id}
-                          className="notification-item"
-                          style={{
-                            padding: "8px 10px",
-                            cursor: "pointer",
-                            transition: "background-color 0.3s ease, color 0.3s ease",
-                            color: "white",
-                          }}
-                          onClick={() => handleNotificationClick(notification._id)}
-                        >
-                          {notification.message}
-                        </div>
-                      ))}
-                      {notifications.length === 0 && (
-                        <p className="no-notifications" style={{ textAlign: "center", fontSize: "14px", padding: "10px" }}>
-                          No new notifications
-                        </p>
-                      )}
-                    </div>
-                  )}
 
                   <NavDropdown
                     title={theaterInfo.name || theaterInfo.data.name}
