a273efe61646e706081c4ca2f04178843b3a1b6c
Adithyan <Adithiruthiparambil12@gmail.com>
2025-02-03 11:47:04 +0530

    Updated PATCH method on server

--- Back-End/server.ts
+++ Back-End/server.ts
@@ -31,7 +31,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
 app.use(express.json());
