197706a6e94d8b541c5c2ad1e6848be428c49643
Adithyan12789 <adithiruthiparambil12@gmail.com>
2026-01-21 15:23:48 +0530

    fix: :bug: Updated the backend connection api

--- src/Core/environment.ts
+++ src/Core/environment.ts
@@ -1,7 +1,7 @@
 const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
 export const config = {
-    apiUrl: process.env.NEXT_PUBLIC_API_URL || (isLocal ? 'http://localhost:5000/' : 'https://ticket-hive-twk0.onrender.com/'),
+    apiUrl: process.env.NEXT_PUBLIC_API_URL || (isLocal ? 'http://localhost:5000/' : 'https://ticket-hive-back-end-1.onrender.com/'),
     isDevelopment: isLocal,
     cookieDomain: isLocal ? 'localhost' : '.tickethive.fun'
 }
--- src/url.ts
+++ src/url.ts
@@ -1,2 +1,2 @@
 const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
-export const backendUrl = isLocal ? "" : "https://ticket-hive-twk0.onrender.com";
+export const backendUrl = isLocal ? "" : "https://ticket-hive-back-end-1.onrender.com";
